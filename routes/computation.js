e5b53478a77d7faf0509c9f7570ba57aa0111be2
anusha kanagala <67909209+Anushakanagala@users.noreply.github.com>
2021-03-24 21:41:04 -0500

    new endpoint

--- routes/computation.js
+++ routes/computation.js
@@ -6,7 +6,7 @@ router.get('/',(req,res)=>{
     let randomNumber=Math.floor(Math.random()*100)+1
     let data=req.query.x
     console.log(data)
-    res.render('compute',{
+    res.render('computation',{
         r:`square root applied to ${randomNumber} is ${Math.sqrt(randomNumber)}`,
         q:`square root applied to ${data} is ${Math.sqrt(data)}`
     })
